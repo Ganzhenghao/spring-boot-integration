d33625a273b9737012ddf5879034dc7bc87c2472
Ganzhenghao <ganzhenghao666@qq.com>
2022-05-05 09:58:34 +0800

    删除.idea 添加bean search模块

---
+++ spring-boot-bean-search-study/spring-boot-bean-search-study/src/main/java/com/example/springbootbeansearchstudy/SpringBootBeanSearchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbeansearchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeanSearchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBeanSearchStudyApplication.class, args);
+    }
+
+}
---
+++ spring-boot-bean-search-study/spring-boot-bean-search-study/src/test/java/com/example/springbootbeansearchstudy/SpringBootBeanSearchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbeansearchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBeanSearchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
